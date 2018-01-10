c525287784c491c926dce8278346c459ee2b9832
Ben Livermore <ben.livermore@intellifylearning.com>
2018-01-09 21:20:14 -0500

    another commit

--- get-versions.sh
+++ get-versions.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+set -e -x
+
 cd ./dummy-repo
-git log `git describe --tags --abbrev=0`..HEAD --oneline
+git log `git describe --tags --abbrev=0`..HEAD --pretty=format:%s > ../commit-messages/list
+
